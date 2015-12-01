ffe94b721676c2412c4ebdf0dc7859a7a11b7290
Heber Mattos <heber.mattos@redehost.local>
2015-12-01 01:16:38 -0200

    env de producao

--- replace_var.sh
+++ replace_var.sh
@@ -4,8 +4,8 @@ dbdatabase = $DB_DATABASE
 debusername = $DB_USERNAME
 dbpassword = $DB_PASSWORD
 
-sed -i 's/%APP_ENV%/${appenv}/g' '.env'
-sed -i 's/%DB_HOST%/${dbhost}/g' '.env'
-sed -i 's/%DB_DATABASE%/${dbdatabase}/g' '.env'
-sed -i 's/%DB_USERNAME%/${debusername}/g' '.env'
-sed -i 's/%DB_PASSWORD%/${dbpassword}/g' '.env'
+sed -i 's/%APP_ENV%/$appenv/g' '.env'
+sed -i 's/%DB_HOST%/$dbhost/g' '.env'
+sed -i 's/%DB_DATABASE%/$dbdatabase/g' '.env'
+sed -i 's/%DB_USERNAME%/$debusername/g' '.env'
+sed -i 's/%DB_PASSWORD%/$dbpassword/g' '.env'
