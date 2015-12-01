bde608f647abdb751ddd7e36ac48a98502dc3f10
Heber Mattos <heber.mattos@redehost.local>
2015-12-01 01:05:46 -0200

    env de producao

--- replace_var.sh
+++ replace_var.sh
@@ -1,6 +1,6 @@
-echo $APP_ENV
-sed -i 's/%APP_ENV%/${APP_ENV}/g' '.env'
-sed -i 's/%DB_HOST%/${DB_HOST}/g' '.env'
-sed -i 's/%DB_DATABASE%/${DB_DATABASE}/g' '.env'
-sed -i 's/%DB_USERNAME%/${DB_USERNAME}/g' '.env'
-sed -i 's/%DB_PASSWORD%/${DB_PASSWORD}/g' '.env'
+
+sed -i 's/%APP_ENV%/$APP_EN}/g' '.env'
+sed -i 's/%DB_HOST%/$DB_HOST/g' '.env'
+sed -i 's/%DB_DATABASE%/$DB_DATABASE/g' '.env'
+sed -i 's/%DB_USERNAME%/$DB_USERNAME/g' '.env'
+sed -i 's/%DB_PASSWORD%/$DB_PASSWORD/g' '.env'
