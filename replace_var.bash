c7246430e11839e608f9a7964cc6e875123f99ea
Heber Mattos <heber.mattos@redehost.local>
2015-12-01 00:14:53 -0200

    env de producao

---
+++ replace_var.bash
@@ -0,0 +1,5 @@
+perl -pi -e 's/%APP_ENV%/$APP_ENV/g' '.env'
+perl -pi -e 's/%DB_HOST%/$DB_HOST/g' '.env'
+perl -pi -e 's/%DB_DATABASE%/$DB_DATABASE/g' '.env'
+perl -pi -e 's/%DB_USERNAME%/$DB_USERNAME/g' '.env'
+perl -pi -e 's/%DB_USERNAME%/$DB_USERNAME/g' '.env'
